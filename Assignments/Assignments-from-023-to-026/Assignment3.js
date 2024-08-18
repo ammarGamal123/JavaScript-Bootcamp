9c4ae2a37eb969f34c30198b820340798e61c6c9
Ammar Hammad <amargamal832909@gmail.com>
2024-08-18 22:12:06 +0300

    solve assignment of if conditions

---
+++ Assignments/Assignments-from-023-to-026/Assignment3.js
@@ -0,0 +1,4 @@
+console.log((Number.MAX_SAFE_INTEGER % (true)) + ((Math.pow((true + true + true + true ) , true + true))));
+
+
+console.log (Number.MAX_SAFE_INTEGER.toString().length);
---
+++ Assignments/Assignments-from-023-to-026/Assignment5.js
@@ -0,0 +1,3 @@
+let num = 10;
+
+console.log (Number.isInteger(10) + Number.isInteger(10));
