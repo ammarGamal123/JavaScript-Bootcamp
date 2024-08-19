3d72adfa734f37de6e369262fc9d21fa6ceaf4fa
Ammar Hammad <amargamal832909@gmail.com>
2024-08-19 23:53:51 +0300

    learn switch statement

---
+++ Lessons/Week07/037-Switch-Statement.js
@@ -0,0 +1,21 @@
+
+
+let day = 2;
+
+switch (day){
+    case 0 :
+        console.log("Saturday");
+        break;
+
+    case 1 :
+        console.log("Sunday");
+        break;
+
+    case 2:    
+    case 3: 
+        console.log("Tuesday");
+        break;
+
+    default:
+        console.log("Unknown day");
+}
