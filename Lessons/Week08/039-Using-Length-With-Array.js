75d5cbb541f07f98a8751aa5c0fa162de8ad1fe1
Ammar Hammad <amargamal832909@gmail.com>
2024-08-22 19:18:47 +0300

    Learn length of the array

---
+++ Lessons/Week08/039-Using-Length-With-Array.js
@@ -0,0 +1,12 @@
+
+let myFriends = ["Ahmed" , "Hamza" , "Yassin" , "Omar"];
+
+console.log(myFriends.length);
+
+myFriends[myFriends.length] = "Anas";
+
+console.log(myFriends);
+
+myFriends.length = 2;
+
+console.log(myFriends);
