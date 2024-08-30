84faaf178da0f8171e07b2db11e327155bb7809c
Ammar Hammad <amargamal832909@gmail.com>
2024-08-30 15:25:01 +0300

    Learn array sorting

---
+++ Lessons/Week08/042-Sorting-Array.js
@@ -0,0 +1,18 @@
+
+
+let myFriends = [10 , "Ammar" , 20 , -10 , "40" , "-23" , "Hammad" , 123 , 23432 , 1000];
+
+console.log(myFriends);
+
+console.log(myFriends.sort());
+
+console.log(myFriends.reverse());
+
+console.log(myFriends.sort().reverse());
+
+let array = [30 , 40 , 10 , -23 , 45 , 1 , 9];
+
+console.log(array.sort()); // array.sort() ==> don't sort as numbers but sort as strings
+
+
+
